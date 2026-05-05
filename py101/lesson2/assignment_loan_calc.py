5ab68ff44bb1861cfb75bcb087b26cdd1157e183
Monica Krueger <monica_krueger@outlook.com>
2026-05-05 12:49:13 -0400

    revised assignment_loan_calc.py, now complete

--- py101/lesson2/assignment_loan_calc.py
+++ py101/lesson2/assignment_loan_calc.py
@@ -1,58 +1,66 @@
+"""Loan Calculator script.
+
+Prompts user to enter the loan amount, APR, duration in months, and returns the monthly payment"""
+
 import json
 
 def messages(message, lang='en'):
+    """returns localized message string from loan_messages.json for the given key"""
     return MESSAGES[lang][message]
 
-with open('loan_messages.json', 'r') as file:
+with open('loan_messages.json', encoding="utf-8") as file:
     MESSAGES = json.load(file)
 
 def prompt(string):
-    print(f'---- {string} ----')
+    """prints trail of hyphens to differentiate between user input and output"""
+    print(f"---- {string} ----")
 
 def invalid_number(number_str):
+    """return True if number_str is not positive, False otherwise"""
     try:
-        float(number_str)
-    except ValueError:
-        return True
-    try:
-        int(number_str)
+        number = float(number_str)
+        if number <= 0:
+            raise ValueError(f"Value must be greater than 0: {number}")
     except ValueError:
         return True
     return False
 
 prompt(messages('welcome'))
+
 while True:
     while True:
         prompt(messages('loan_prompt'))
-        loan_amount = float(input())
+        loan_amount = input()
         if not invalid_number(loan_amount):
             break
-        prompt(messages('invalid_prompt'))
+        prompt(messages('invalid_number'))
 
     while True:
         prompt(messages('apr'))
-        apr = float(input())
+        apr = input()
         if not invalid_number(apr):
             break
         prompt(messages('invalid_apr'))
 
-    apr = apr * .01
+    apr = float(apr) * .01
 
     while True:
         prompt(messages('loan_dur_months'))
-        loan_duration = float(input())
+        loan_duration = input()
         if not invalid_number(loan_duration):
             break
         prompt(messages('invalid_number'))
 
     monthly_ir = apr / 12
+    loan_amount = float(loan_amount)
+    loan_duration = float(loan_duration)
+
+
     monthly_pmt = loan_amount * (monthly_ir / (1 - (1 + monthly_ir) ** (-loan_duration)))
 
-    output = round(monthly_pmt, 2)
-
-    prompt(f'{messages('monthly_pmt')} ${output}')
+    prompt(f"{messages('monthly_pmt')} ${monthly_pmt:.2f}")
 
     prompt(messages('another_calc'))
     answer = input()
-    if answer and answer[0].lower() != 'y':
+    if answer and answer[0].lower() not in ('y', 'o', 's'):
         break
